3c77edeb0228bfbe6dab74b0c3671d775df15a2e
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2025-06-10 04:46:02 -0300

    comitando missaoModel

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 // Ele transforma uma classe em uma entidade de banco de dados
 @Entity
@@ -13,6 +16,7 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
+    private List<MissaoModel> missoes;
 
     public NinjaModel() {
     }
