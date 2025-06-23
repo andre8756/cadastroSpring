d0348b82d67623281a1fde40ce978e2aea97cab8
Adnré Herberto Schmitt <schmitt.andre23@gmail.com>
2025-06-23 04:28:24 -0300

    implementando service

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -3,6 +3,9 @@ package dev.java10x.CadastroDeNinjas.Ninjas;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class NinjaService {
 
@@ -11,6 +14,18 @@ public class NinjaService {
 
     public NinjaModel salvar(NinjaModel ninjaModel){
         return ninjaRepository.save(ninjaModel);
+    }
+
+    public List<NinjaModel> listarNinjas(){
+        return ninjaRepository.findAll();
+    }
+
+    public Optional<NinjaModel> buscarPorId(Long id){
+        return ninjaRepository.findById(id);
+    }
+
+    public void removerPorId(Long id){
+        ninjaRepository.deleteById(id);
     }
 
 }
